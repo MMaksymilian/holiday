c47ae9a5616ab5d33c138900944eb0e41fcd86bc
Maksymilian <maksymilian.malek@primaris.eu>
2012-07-23 14:34:51 +0200

    intial commit

---
+++ server/src/main/java/test/primaris/controller/TestController.java
@@ -0,0 +1,23 @@
+package test.primaris.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: USER
+ * Date: 23.07.12
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping("hello")
+public class TestController {
+
+    @RequestMapping(method= RequestMethod.GET)
+    public String get(ModelMap model) {
+        return "index";
+    }
+}
